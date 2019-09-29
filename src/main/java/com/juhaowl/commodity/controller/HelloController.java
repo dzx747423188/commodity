f5b8c106b4fb6db6c9f34dcc19ec7d31f16291dd
park <747423188@qq.com>
2019-09-29 15:22:40 +0800

    first commit init project

---
+++ src/main/java/com/juhaowl/commodity/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.juhaowl.commodity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Park on 2019-9-26.
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam (name = "name",required = false,defaultValue = "world")String name , Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
